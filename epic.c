7f5bb3000684378bbf1d884bd064a549fb67ea83
Hugh Rawlinson <hughr2005@gmail.com>
2023-06-25 15:03:25 +0200

    Complete curl downloading images

--- epic.c
+++ epic.c
@@ -1,5 +1,6 @@
 #define CURL_STATICLIB
-#define DRAW_GUI
+#define _GNU_SOURCE
+// #define DRAW_GUI
 #include "curl/curl.h"
 #include "gui.c"
 #include <json-c/json_tokener.h>
@@ -17,6 +18,11 @@ struct response {
   char *ptr;
   size_t len;
 };
+
+typedef struct image_meta {
+  char *url[85];
+  char *id[22];
+} IMAGE_META;
 
 void init_response(struct response *s) {
   s->len = 0;
@@ -42,14 +48,16 @@ size_t curl_write_cb(void *ptr, size_t size, size_t nmemb, struct response *s) {
   return size * nmemb;
 }
 
-struct response get_epic_images() {
+struct response get_epic_image_manifest() {
+  struct response chunk;
+
   CURL *curl;
+  CURLcode res;
   curl = curl_easy_init();
   curl_easy_setopt(curl, CURLOPT_URL, EPIC_API_ENDPOINT);
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_cb);
 
-  struct response chunk;
   init_response(&chunk);
 
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
@@ -59,7 +67,7 @@ struct response get_epic_images() {
     exit(1);
   }
 
-  CURLcode res = curl_easy_perform(curl);
+  res = curl_easy_perform(curl);
 
   if (res != CURLE_OK) {
     fprintf(stderr, "Failed to load epic images: %s\n",
@@ -69,6 +77,65 @@ struct response get_epic_images() {
 
   curl_easy_cleanup(curl);
   return chunk;
+}
+
+static const char *datafolder = "/home/hugh/epic-images/";
+
+void start_epic_image_download(IMAGE_META *image_metas, size_t count) {
+  CURL *handles[count];
+  CURLM *multi_handle = curl_multi_init();
+  CURLMsg *msg;  /* for picking up messages with the transfer status */
+  int msgs_left; /* how many messages are left */
+  FILE *of;
+  int still_running = 1;
+  char *output_filepath;
+
+  for (size_t i = 0; i < count; i++) {
+    handles[i] = curl_easy_init();
+    output_filepath =
+        malloc(strlen(datafolder) + strlen(image_metas[i].id) + 5);
+    strcpy(output_filepath, datafolder);
+    strcat(output_filepath, image_metas[i].id);
+    strcat(output_filepath, ".png\0");
+    of = fopen(output_filepath, "wb");
+    printf("%s\n", image_metas[i].url);
+    curl_easy_setopt(handles[i], CURLOPT_URL, image_metas[i].url);
+    curl_easy_setopt(handles[i], CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(handles[i], CURLOPT_WRITEDATA, of);
+    curl_multi_add_handle(multi_handle, handles[i]);
+    printf("%s\n", output_filepath);
+    free(output_filepath);
+  }
+
+  while (still_running) {
+    printf("%d\n", still_running);
+    CURLMcode mc = curl_multi_perform(multi_handle, &still_running);
+    printf("%d\n", still_running);
+
+    if (still_running) {
+      printf("Still running: %d", still_running);
+      mc = curl_multi_poll(multi_handle, NULL, 0, 1000, NULL);
+    }
+
+    if (mc) {
+      fprintf(stderr, "CURL Multi Poll response: %s", curl_multi_strerror(mc));
+      break;
+    }
+    sleep(1);
+  }
+
+  while ((msg = curl_multi_info_read(multi_handle, &msgs_left))) {
+    if (msg->msg == CURLMSG_DONE) {
+      printf("HTTP transfer completed with status %s\n",
+             curl_easy_strerror(msg->data.result));
+    }
+
+    for (size_t i = 0; i < count; i++) {
+      curl_multi_remove_handle(multi_handle, handles[i]);
+      curl_easy_cleanup(handles[i]);
+    }
+    curl_multi_cleanup(multi_handle);
+  }
 }
 
 json_object *parse_response_body(struct response *r) {
@@ -91,36 +158,51 @@ json_object *parse_response_body(struct response *r) {
   return jobj;
 }
 
-int main(void) {
-  curl_global_init(CURL_GLOBAL_ALL);
+void get_image_metas_from_manifest(IMAGE_META *image_metas, size_t image_count,
+                                   struct json_object *body) {
+  char year[5];
+  char month[3];
+  char day[3];
 
-  struct response r = get_epic_images();
-  json_object *body = parse_response_body(&r);
-
-  size_t arr = json_object_array_length(body);
-  for (size_t i = 0; i < arr; i++) {
+  for (size_t i = 0; i < image_count; i++) {
     json_object *photo = json_object_array_get_idx(body, i);
     json_object *image_name_object = json_object_object_get(photo, "image");
     const char *image_name = json_object_get_string(image_name_object);
 
-    char year[5];
     strncpy(year, image_name + 8, 4);
     year[4] = '\0';
-    char month[3];
     strncpy(month, image_name + 12, 2);
     month[2] = '\0';
-    char day[3];
     strncpy(day, image_name + 14, 2);
     day[2] = '\0';
 
-    printf("https://epic.gsfc.nasa.gov/archive/natural/%s/%s/%s/png/%s.png\n",
-           year, month, day, image_name);
+    snprintf(image_metas[i].url, 85,
+             "https://epic.gsfc.nasa.gov/archive/natural/%s/%s/%s/png/%s.png",
+             year, month, day, image_name);
+    strcpy(image_metas[i].id, image_name);
   }
+}
+
+int main(void) {
+  struct response r;
+  json_object *body;
+  size_t image_count;
+  IMAGE_META *image_metas;
+  int draw_result;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  r = get_epic_image_manifest();
+  body = parse_response_body(&r);
+  image_count = json_object_array_length(body);
+  image_metas = malloc(sizeof(IMAGE_META) * image_count);
+  get_image_metas_from_manifest(image_metas, image_count, body);
+  start_epic_image_download(image_metas, image_count);
 
   free(body);
   free(r.ptr);
 
-  int draw_result = draw();
+  draw_result = draw();
 
   curl_global_cleanup();
   return draw_result;
